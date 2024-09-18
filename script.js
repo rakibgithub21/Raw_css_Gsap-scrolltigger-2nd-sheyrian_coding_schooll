e30ea12c2e0b2ced4d703ce212a72f5475151b67
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-09-18 09:17:23 +0600

    first gsap scroll trigger use

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ gsap.from('#page2 #box', {
     scale: 0,
     duration: 2,
     rotate: 360,
+    // scrollTrigger:"#page2 #box"
+    //here #page2 #box is target which container i want to add scroll trigger
+    // just normal scroll trigger
+    scrollTrigger: {
+        // complex and best
+        trigger: "#page2 #box",
+        scroller: "body",
+        markers: true,
+        start: 'top 60%',
+        
+        
+    }
 })
