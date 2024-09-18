bb39da0b0c09fba9bb419ba607f51586a7002500
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-09-18 09:40:42 +0600

    another practive

--- script.js
+++ script.js
@@ -24,25 +24,45 @@ gsap.from('#page1 #box', {
 
 // another
 
-gsap.from('#page2 h1', {
-    opacity: 0,
-    x:500,
-    duration: 2,
-    scrollTrigger: {
-        trigger: "#page2 h1",
-        scroller: 'body',
-        start: 'top 50%',
-        markers:true,
-}
-})
-gsap.from('#page2 h2', {
-    opacity: 0,
-    x:-500,
-    duration: 2,
-    scrollTrigger: {
-        trigger: "#page2 h1",
-        scroller: 'body',
-        start: 'top 50%',
-        markers:true,
-}
-})
+// gsap.from('#page2 h1', {
+//     opacity: 0,
+//     x:500,
+//     duration: 2,
+//     scrollTrigger: {
+//         trigger: "#page2 h1",
+//         scroller: 'body',
+//         start: 'top 50%',
+//         markers:true,
+// }
+// })
+// gsap.from('#page2 h2', {
+//     opacity: 0,
+//     x:-500,
+//     duration: 2,
+//     scrollTrigger: {
+//         trigger: "#page2 h1",
+//         scroller: 'body',
+//         start: 'top 50%',
+//         markers:true,
+// }
+// })
+
+
+
+//another practice
+
+// gsap.from('#page3 #box', {
+//     scale: 0,
+//     opacity: 0,
+//     duration: 2,
+//     rotate:740,
+//     scrollTrigger: {
+//         trigger: "#page3 #box",
+//         scroller: 'body',
+//         start: 'top 60%',
+//         end:'top 30%',
+//         markers: true,
+//         scrub: 5,  //scrub is allowd (true or false and 1 - 5)
+//         pin:true // explore what is pin
+//     }
+// })
